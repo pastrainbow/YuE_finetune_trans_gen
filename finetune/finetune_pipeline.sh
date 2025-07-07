35f4c57f11645c6c3990a1eb6e93d5859b0ebf7a
pastrainbow <adamliofficial@gmail.com>
2025-07-07 14:54:59 +0100

    first commit

---
+++ finetune/finetune_pipeline.sh
@@ -0,0 +1,9 @@
+#clear previous log and binary files
+rm -rf count_token_logs
+rm -rf example/mmap
+#preprocess dataset, then start finetuning
+bash scripts/preprocess_data.sh trans_gen cot
+bash scripts/preprocess_data.sh trans_gen icl_cot
+bash scripts/count_tokens.sh ./example/mmap/
+python core/parse_mixture.py -c example/trans_gen_data_mixture_cfg.yml
+bash scripts/run_finetune.sh
