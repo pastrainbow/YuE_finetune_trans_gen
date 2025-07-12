15d736532c5b49fc1449e638939a491c58f2d49a
pastrainbow <adamliofficial@gmail.com>
2025-07-12 09:05:50 +0100

    Updated the check_sep

--- preprocess_dataset/check_sep_progress.py
+++ preprocess_dataset/check_sep_progress.py
@@ -8,8 +8,6 @@ for input_track_path in input_track_paths:
     name = Path(input_track_path).stem
     vocals_path = os.path.join(output_dir_path, name + ".Vocals.mp3")
     instr_path = os.path.join(output_dir_path, name + ".Instrumental.mp3")
-    if (not (os.path.exists(vocals_path) and os.path.exists(instr_path))):
-        print(f"File {name} did not get separated.")
-    else:
+    if (os.path.exists(vocals_path) and os.path.exists(instr_path)):
         file_count += 1
 print(f"{file_count} files got separated.")
