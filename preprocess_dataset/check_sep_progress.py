c2e9b53f3542dc5d194d311f9e40c22a90a34cc2
pastrainbow <adamliofficial@gmail.com>
2025-07-12 08:26:15 +0100

    Added script for checking vocal separation progress

---
+++ preprocess_dataset/check_sep_progress.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+input_dir_path = "/vol/bitbucket/al4624/finetune_dataset/fma_large/"
+output_dir_path = "/vol/bitbucket/al4624/finetune_dataset/fma_large_sep/"
+input_track_paths = [str(file) for file in Path(input_dir_path).rglob('*.mp3') if file.is_file()]
+output_track_paths =[str(file) for file in Path(output_dir_path).rglob('*.mp3') if file.is_file()]
+for input_track_path in input_track_paths:
+    name = os.path.basename(input_track_path)
+    vocals_path = os.path.join(output_dir_path, name, ".Vocals.mp3")
+    instr_path = os.path.join(output_dir_path, name, ".Instrumental.mp3")
+    if (not (os.path.exists(vocals_path) and os.path.exists(instr_path))):
+        print(f"File {name} did not get seperated.")
