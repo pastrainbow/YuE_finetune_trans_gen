99429415702d9b74bd22f7ce3ea9e0ee205e7367
Adam Li <al4624@ic.ac.uk>
2025-07-30 11:53:49 +0100

    Track files

--- finetune/scripts/run_finetune.sh
+++ finetune/scripts/run_finetune.sh
@@ -115,11 +115,11 @@ PER_DEVICE_EVAL_BATCH_SIZE=1
 GLOBAL_BATCH_SIZE=$((NUM_GPUS*PER_DEVICE_TRAIN_BATCH_SIZE))
 USE_BF16=true
 SEQ_LENGTH=8192
-TRAIN_ITERS=9777
+TRAIN_ITERS=4894
 NUM_TRAIN_EPOCHS=5
 
 # Data paths (replace with your actual paths)
-DATA_PATH="640775634 ./example/mmap/trans_gen.msa.xcodec_16k_stage_1_token_level_interleave_long_prompt_msa_textfirst_inst_text_document"
+DATA_PATH="320787029 ./example/mmap/trans_gen.msa.xcodec_16k_stage_1_token_level_interleave_long_prompt_msa_textfirst_inst_text_document"
 DATA_CACHE_PATH="/vol/bitbucket/al4624/data_cache"
 
 # Set comma-separated list of proportions for training, validation, and test split
